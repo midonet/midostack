5670936b9a9e425259d0f5039491e95284f2227e
Tomoe Sugihara <tomoe@midokura.com>
2014-05-30 09:22:07 +0000

    Move out installing dependencies part to a hook script

--- midonet_stack.sh
+++ midonet_stack.sh
@@ -52,12 +52,6 @@ done
 
 if [[ "$os_VENDOR" =~ (Red Hat) || "$os_VENDOR" =~ (CentOS) ]]; then
 
-    sudo yum install -y dsc1.1 zookeeper screen git curl java-1.7.0-openjdk
-
-    sudo yum install -y python-setuptools dbus gcc python-devel libxslt-devel libxml2-devel
-    # RHEL does not provide this package, installing directly from centos
-    sudo yum install -y http://mirror.centos.org/centos-6/6/os/x86_64/Packages/libffi-devel-3.0.5-3.2.el6.x86_64.rpm
-
     #Iptables disabled for now
     sudo service iptables stop
 
@@ -79,9 +73,6 @@ if [[ "$os_VENDOR" =~ (Red Hat) || "$os_VENDOR" =~ (CentOS) ]]; then
 
 
 elif [[ "$os_VENDOR" =~ (Ubuntu) || "$os_VENDOR" =~ (Debian) ]]; then
-
-    # Install dependences
-    sudo apt-get install -y python-dev libxml2-dev libxslt-dev openjdk-7-jdk openjdk-7-jre zookeeper zookeeperd cassandra openvswitch-datapath-dkms linux-headers-`uname -r` maven screen git curl
 
     # Configure casandra
     sudo service cassandra stop
