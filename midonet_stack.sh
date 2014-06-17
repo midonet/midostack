3985863739f6a0ee32b766a2150aa16c6faba42c
Tomoe Sugihara <tomoe@midokura.com>
2014-06-17 08:22:53 +0000

    Move out MidoNet API setting to a pre_stack hook

--- midonet_stack.sh
+++ midonet_stack.sh
@@ -68,23 +68,6 @@ if [ $BUILD_SOURCES = true ]; then
 
 
 
-    # Set up web.xml for midonet-api
-    MIDONET_API_CFG=$MIDONET_SRC_DIR/midonet-api/src/main/webapp/WEB-INF/web.xml
-    cp $MIDONET_API_CFG.dev $MIDONET_API_CFG
-    cp $MIDONET_API_CFG.dev $MIDONET_API_CFG.bak
-    # TODO(ryu): Improve this part
-    sed -i -e "s/999888777666/$PASSWORD/g" $MIDONET_API_CFG
-    sed -i -e "s/mido_admin/admin/g" $MIDONET_API_CFG
-    sed -i -e "s/mido_tenant_admin/Member/g" $MIDONET_API_CFG
-    sed -i -e "s/mido_tenant_user/Member/g" $MIDONET_API_CFG
-    if [ $MIDONET_API_USE_KEYSTONE = true ]; then
-        sed -i -e "s/org.midonet.api.auth.MockAuthService/org.midonet.api.auth.keystone.v2_0.KeystoneService/g" $MIDONET_API_CFG
-    fi
-
-    sed -i -e "/<param-name>keystone-service_host<\/param-name>/{n;s%.*%    <param-value>$KEYSTONE_AUTH_HOST</param-value>%g}" $MIDONET_API_CFG
-    sed -i -e "/<param-name>keystone-admin_token<\/param-name>/{n;s%.*%    <param-value>$ADMIN_PASSWORD</param-value>%g}" $MIDONET_API_CFG
-    cp $MIDONET_API_CFG $MIDONET_API_CFG.bak
-    cat $MIDONET_API_CFG
 
 
     # Install control panel
@@ -134,21 +117,6 @@ else
 
         stop_service $TOMCAT
     fi
-
-    # Set up web.xml for midonet-api
-    MIDONET_API_CFG=/usr/share/midonet-api/WEB-INF/web.xml
-    sudo cp $MIDONET_API_CFG.dev $MIDONET_API_CFG
-    sudo cp $MIDONET_API_CFG.dev $MIDONET_API_CFG.bak
-    # TODO(ryu): Improve this part
-    sudo sed -i -e "s/999888777666/$PASSWORD/g" $MIDONET_API_CFG
-    sudo sed -i -e "s/mido_admin/admin/g" $MIDONET_API_CFG
-    sudo sed -i -e "s/mido_tenant_admin/Member/g" $MIDONET_API_CFG
-    sudo sed -i -e "s/mido_tenant_user/Member/g" $MIDONET_API_CFG
-    sudo sed -i -e "s/org.midonet.api.auth.MockAuthService/org.midonet.api.auth.keystone.v2_0.KeystoneService/g" $MIDONET_API_CFG
-    sudo sed -i -e "/<param-name>keystone-service_host<\/param-name>/{n;s%.*%    <param-value>$KEYSTONE_AUTH_HOST</param-value>%g}" $MIDONET_API_CFG
-    sudo sed -i -e "/<param-name>keystone-admin_token<\/param-name>/{n;s%.*%    <param-value>$ADMIN_PASSWORD</param-value>%g}" $MIDONET_API_CFG
-    sudo cp $MIDONET_API_CFG $MIDONET_API_CFG.dev
-
 fi
 
 # Start midonet-api and midolman in a screen
---
+++ pre_devstack.d/150.build_midonet_api.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+
+
+if [ $BUILD_SOURCES = true ]; then
+    # Set up web.xml for midonet-api
+    MIDONET_API_CFG=$MIDONET_SRC_DIR/midonet-api/src/main/webapp/WEB-INF/web.xml
+else
+    # Set up web.xml for midonet-api
+    MIDONET_API_CFG=/usr/share/midonet-api/WEB-INF/web.xml
+fi
+
+
+sudo cp $MIDONET_API_CFG.dev $MIDONET_API_CFG
+sudo cp $MIDONET_API_CFG.dev $MIDONET_API_CFG.bak
+# TODO(ryu): Improve this part
+sudo sed -i -e "s/999888777666/$PASSWORD/g" $MIDONET_API_CFG
+sudo sed -i -e "s/mido_admin/admin/g" $MIDONET_API_CFG
+sudo sed -i -e "s/mido_tenant_admin/Member/g" $MIDONET_API_CFG
+sudo sed -i -e "s/mido_tenant_user/Member/g" $MIDONET_API_CFG
+
+if [ $MIDONET_API_USE_KEYSTONE = true ]; then
+    sudo sed -i -e "s/org.midonet.api.auth.MockAuthService/org.midonet.api.auth.keystone.v2_0.KeystoneService/g" $MIDONET_API_CFG
+fi
+sudo sed -i -e "/<param-name>keystone-service_host<\/param-name>/{n;s%.*%    <param-value>$KEYSTONE_AUTH_HOST</param-value>%g}" $MIDONET_API_CFG
+sudo sed -i -e "/<param-name>keystone-admin_token<\/param-name>/{n;s%.*%    <param-value>$ADMIN_PASSWORD</param-value>%g}" $MIDONET_API_CFG
