568c2b9256a0e6560913f8129071a14b3f14bd1b
Tomoe Sugihara <tomoe@midokura.com>
2014-07-15 09:15:10 +0000

    Install pip

--- hooks/pre_devstack.d/000.install_prerequisites.sh
+++ hooks/pre_devstack.d/000.install_prerequisites.sh
@@ -3,4 +3,4 @@
 # Install all prerequisites of midostack
 #
 sudo apt-get update
-sudo apt-get install -y --force-yes git python screen openjdk-7-jdk
+sudo apt-get install -y --force-yes git python screen openjdk-7-jdk python-pip
