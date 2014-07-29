d322151b2ac241b773dc59d0141dfbceb349e221
Tomoe Sugihara <tomoe@midokura.com>
2014-07-29 03:03:06 +0000

    Exit when stack.sh fails
    Fixes: MN-2620

--- midonet_stack.sh
+++ midonet_stack.sh
@@ -89,7 +89,10 @@ echo Executing vanilla stack.sh script in devstack...
 echo Logfile: $LOGFILE
 echo ================================================
 cp $MIDOSTACK_TOPDIR/devstackrc $DEVSTACK_DIR/local.conf
-cd $DEVSTACK_DIR && ./stack.sh
+cd $DEVSTACK_DIR && ./stack.sh || {
+    echo stack.sh failed. Exiting...
+    exit 1
+}
 
 # save vanilla devstack logs
 
