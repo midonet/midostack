012d13b9ccf12e090214e68326df805498e8a2d5
Tomoe Sugihara <tomoe@midokura.com>
2014-10-24 08:28:38 +0000

    Install midonet-cli
    Change-Id: Ic243aa839ce472dcb89927cbd95bc650cc1163e9
    Implement:  MN-3019

---
+++ scripts/post_devstack.d/040.install_midonet_cli.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+# Copyright 2014 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MIDO_DEST=${MIDO_DEST:-/opt/stack/midonet}
+MIDONET_CLIENT_DIR=${MIDO_DEST}/python-midonetclient
+
+# Clean up previous installation
+sudo rm -rf /usr/local/bin/midonet-cli /usr/local/lib/python2.7/dist-packages/midonetclient*
+
+# Install python module and midonet-cli
+sudo apt-get install ncurses-dev
+sudo pip install readline
+cd $MIDONET_CLIENT_DIR
+sudo python setup.py install
+
+
