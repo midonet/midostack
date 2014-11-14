6ffba1475754316bb410a6fd8e7ae0d678ab8d02
Tomoe Sugihara <tomoe@midokura.com>
2014-11-14 14:02:37 +0000

    Use new config in cassandra to avoid prompting
    Change-Id: I8f1b870f8e0613586a1a68f69d6522c64a5bd8f4

--- scripts/pre_devstack.d/120.cassandra.sh
+++ scripts/pre_devstack.d/120.cassandra.sh
@@ -19,7 +19,7 @@
 CASSANDRA_FILE='/etc/cassandra/cassandra.yaml'
 CASSANDRA_ENV_FILE='/etc/cassandra/cassandra-env.sh'
 
-sudo apt-get -y --force-yes install dsc21
+sudo apt-get -o Dpkg::Options::="--force-confnew" -y --force-yes install dsc21
 
 sudo service cassandra stop
 sudo chown cassandra:cassandra /var/lib/cassandra
