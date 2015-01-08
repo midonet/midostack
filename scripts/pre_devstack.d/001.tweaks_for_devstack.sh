bb462ab579379de632f6bc28e41637c04bcede10
Jaume Devesa <devvesa@gmail.com>
2015-01-08 17:16:16 +0000

    Don't apply any patch for master
    The two patches applied for juno:
    * Change the midonet namespace plugin name
    * Add the extension path module
    
    Is not needed anymore since the plugin decomposition work
    
    Change-Id: I7917c051d84267974a3a10f281f060918d7552a1

--- scripts/pre_devstack.d/001.tweaks_for_devstack.sh
+++ scripts/pre_devstack.d/001.tweaks_for_devstack.sh
@@ -28,9 +28,12 @@ fi
 
 if [ $MIDOSTACK_NEUTRON_PLUGIN_LOCATION == "downstream" ] ; then
     export DHCP_DRIVER="midonet.neutron.agent.midonet_driver.DhcpNoOpDriver"
-    if [ $MIDOSTACK_OPENSTACK_BRANCH == "master" ]  || [ $MIDOSTACK_OPENSTACK_BRANCH == "stable/juno" ] ; then
+    if [ $MIDOSTACK_OPENSTACK_BRANCH == "stable/juno" ] ; then
         patch -N -d $DEVSTACK_DIR -p1 < $PATCHES_DIR/downstream_plugin_with_juno.patch
         patch -N -d $DEVSTACK_DIR -p1 < $PATCHES_DIR/add_extensions_path.patch
+    elif [ $MIDOSTACK_OPENSTACK_BRANCH == "master" ] ; then
+        # Don't apply any patch for master branch.
+        :
     else
         patch -N -d $DEVSTACK_DIR -p1 < $PATCHES_DIR/devstack-use-downstream-neutron-plugin.patch
     fi
